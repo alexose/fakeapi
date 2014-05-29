d6c2238302ebcc4420a3da3e87a11be9bca8c29f
Alexander Ose <alex@crimsonhexagon.com>
2014-05-29 14:38:32 -0400

    router list improvement

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var routes = require('../routes');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+
+    var data = {
+        title: 'Express',
+        routes : routes.routes
+    }
+
+  res.render('index', data);
 });
 
 module.exports = router;
