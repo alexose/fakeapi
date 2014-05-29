8a86364f729b48f82f0bbada30cef0559659109c
Alexander Ose <alex@crimsonhexagon.com>
2014-05-29 13:51:08 -0400

    initial scaffold

---
+++ ch.js
@@ -0,0 +1,15 @@
+// Crimson Hexagon Node.js API Client
+
+var CH = function(){
+
+}
+
+CH.prototype.auth = function(callback){
+
+}
+
+CH.prototype.get  = function(callback){
+
+}
+
+module.exports = new CH();
